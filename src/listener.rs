6d618f3fbc2e9c7e0c0ed15fd1e41c3c12c19883
dimstyle <smilehacking27@gmail.com>
2025-09-10 08:41:07 +0700

    adding listener machine

--- src/listener.rs
+++ src/listener.rs
@@ -1,10 +1,13 @@
+use std::fmt::format;
 
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener};
 
-async fn listen_from(ip: &str, port: i32){
+use crate::listener;
 
+pub async fn listen_from(ip: &str, port: i32, listenOnTcp: bool, listenOnUdp: bool){
 
-    let listener = TcpListener::bind(format!("{}:{}",ip,port)).await.unwrap();
-    // f
+   let listener = TcpListener::bind(format!("{}:{}",ip, port)).await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap().ip());
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,29 @@ use clap::{ArgAction, Parser};
 #[derive(Parser)]
 struct Args{
     #[arg(short, long, action = ArgAction::SetTrue)]
-    version: bool
-}
+    version: bool,
+     
+     #[arg(short, long, action = ArgAction::SetTrue)]
+    tcp: bool,
+
+     #[arg(short, long, action = ArgAction::SetTrue)]
+    udp: bool,
+
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    listen: bool
+}   
 
 #[tokio::main]
-async fn main(){
+async fn main() -> Result<(),Box<dyn std::error::Error>>{
     let args = Args::parse();
 
-    if (args.version){
+    if args.version{
         println!("v0.0.1");
     }
+
+    if args.listen{
+        listener::listen_from("0.0.0.0",4000, args.tcp, args.udp).await;
+    }
+
+    Ok(())
 }
