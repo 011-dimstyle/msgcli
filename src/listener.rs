4197542cd328317ce9f6cc67ed75b8f8c07b94b4
dimstyle <smilehacking27@gmail.com>
2025-09-15 23:18:11 +0700

    try using axum

--- src/listener.rs
+++ src/listener.rs
@@ -1,17 +1,23 @@
 use std::fmt::format;
 
-use tokio::net::{TcpListener};
+use tokio::{io::{AsyncWriteExt}, net::{TcpListener}};
 
 pub async fn listen_from(
     ip: &str, 
     port: i32, 
     listenOnTcp: bool, 
-    listenOnUdp: bool
+    listenOnUdp: bool,
+    message: String
 )-> Result<(), Box<dyn std::error::Error>>{
 
     let listener = TcpListener::bind(format!("{}:{}",ip, port)).await?;
     println!("listening on {}", listener.local_addr()?.ip());
-
+    let (mut stream, socks) = listener.accept().await?;
+    println!("accept socket from : {}",socks);
+    if message.len() > 0{ 
+        stream.write_all(message.as_bytes()).await?;
+        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    }
     Ok(())
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,57 @@
-mod listener;
-mod test;
-use clap::{ArgAction, Parser};
+// mod listener;
+// mod test;
 
+// use std::{fs::read, io::Read};
 
-#[derive(Parser)]
-struct Args{
-    #[arg(short, long, action = ArgAction::SetTrue)]
-    version: bool,
+// use clap::{ArgAction, Parser, command};
+
+// #[derive(Parser)]
+// #[command(disable_help_flag = true)]
+// struct Args{
+//     #[arg(short, long, action = ArgAction::SetTrue)]
+//     version: bool,
      
-     #[arg(short, long, action = ArgAction::SetTrue)]
-    tcp: bool,
+//      #[arg(short, long, action = ArgAction::SetTrue)]
+//     tcp: bool,
 
-     #[arg(short, long, action = ArgAction::SetTrue)]
-    udp: bool,
+//      #[arg(short, long, action = ArgAction::SetTrue)]
+//     udp: bool,
 
-    #[arg(short, long, action = ArgAction::SetTrue)]
-    listen: bool,
+//     #[arg(short, long, action = ArgAction::SetTrue)]
+//     listen: bool,
 
-    #[arg(short, long)]
-    host: String,
+//     #[arg(short, long, default_value_t = String::default())]
+//     host: String,
 
-    #[arg(short, long)]
-    port: i32
-}   
+//     #[arg(short, long, default_value_t = 0i32)]
+//     port: i32,
+
+//     #[arg(short, long, default_value_t = String::default())]
+//     message: String
+// }   
+
+// #[tokio::main]
+// async fn main() -> Result<(),Box<dyn std::error::Error>>{
+//     let args = Args::parse();
+
+//     if args.version{
+//         println!("v0.0.1");
+//     }
+
+//     if args.listen{
+//         listener::listen_from(&args.host,args.port, args.tcp, args.udp,args.message).await?;
+//     }
+
+//     Ok(())
+
+// }
+
+use axum::{routing::get, Router};
 
 #[tokio::main]
-async fn main() -> Result<(),Box<dyn std::error::Error>>{
-    let args = Args::parse();
+async fn main(){
+    let app: Router = Router::new()
+        .route("/", get(async || {"Hello, World!"}));
 
-    if args.version{
-        println!("v0.0.1");
-    }
-
-    if args.listen{
-        listener::listen_from(&args.host,args.port, args.tcp, args.udp).await;
-    }
-
-    Ok(())
+    axum::serve(tokio::net::TcpListener::bind("127.0.0.1:2000").await.unwrap(),app.into_make_service()).await.unwrap();
 }
