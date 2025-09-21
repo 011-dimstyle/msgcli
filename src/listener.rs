91216dd49ba258866f25178f7d0c16a3eaccbbf4
dimstyle <smilehacking27@gmail.com>
2025-09-21 18:44:34 +0700

    add streaming listener

--- src/listener.rs
+++ src/listener.rs
@@ -1,23 +1,65 @@
-use std::fmt::format;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio::{
+    io::{AsyncWriteExt, BufReader, AsyncBufReadExt},
+    net::{TcpListener},
+};
 
-use tokio::{io::{AsyncWriteExt}, net::{TcpListener}};
+pub async fn listening(host: String, port: u32) -> Result<(), Box<dyn std::error::Error>> {
 
-pub async fn listen_from(
-    ip: &str, 
-    port: i32, 
-    listenOnTcp: bool, 
-    listenOnUdp: bool,
-    message: String
-)-> Result<(), Box<dyn std::error::Error>>{
+    let listen = TcpListener::bind(format!("{}:{}", host, port)).await?;
+    println!("listen on {}", listen.local_addr()?);
+    
+    let (streammutex, ipaddr) = match listen.accept().await{
+        Ok((stream,sock)) => (Arc::new(Mutex::new(stream)), Arc::new(Mutex::new(sock))),
+        Err(e) => {panic!("{}", e);}
+    };
+    
+    let streamread = Arc::clone(&streammutex);
+    let streamwrite = Arc::clone(&streammutex);
+    let ipaddrmutex = Arc::clone(&ipaddr);
+    
+    let writerhandler = tokio::spawn(async move {
 
-    let listener = TcpListener::bind(format!("{}:{}",ip, port)).await?;
-    println!("listening on {}", listener.local_addr()?.ip());
-    let (mut stream, socks) = listener.accept().await?;
-    println!("accept socket from : {}",socks);
-    if message.len() > 0{ 
-        stream.write_all(message.as_bytes()).await?;
-        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-    }
+        let mut streamguard = streamwrite.lock().await;
+        let input = tokio::io::stdin();
+        let mut output = tokio::io::stdout();
+        let readerbuffer = BufReader::new(input);
+        let mut lines = readerbuffer.lines();
+        loop{
+            output.write_all(format!("{} : ", streamguard.local_addr().unwrap()).as_bytes()).await.unwrap();
+            output.flush().await.unwrap();
+            match lines.next_line().await.unwrap() {
+                Some(mut line) => {
+                    line.push('\n');
+                    streamguard.write_all(line.as_bytes()).await.unwrap();
+                },
+                None => {
+                    println!("");
+                    break;
+                }
+            }
+        }   
+                                                                    
+    });
+        
+    tokio::spawn(async move {
+        println!("hello");
+        let mut streamguard = streamread.lock().await;
+        let mut readstring = String::new();
+        let mut readerbuffer = BufReader::new(&mut *streamguard);
+        let mut output = tokio::io::stdout();
+        let ipaddrguard = ipaddrmutex.lock().await;
+        loop{
+            // streamguard.read_to_string(&mut readbuffer).await.unwrap();
+            readerbuffer.read_line(&mut readstring).await.unwrap();
+            output.write_all(format!("{} : {} ", ipaddrguard ,readstring).as_bytes()).await.unwrap();
+            output.flush().await.unwrap();
+        }
+    });
+
+    writerhandler.await?;
+
     Ok(())
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,48 @@
-// mod listener;
-// mod test;
+mod listener;
+mod test;
 
-// use std::{fs::read, io::Read};
+use std::{fs::read, io::Read};
 
-// use clap::{ArgAction, Parser, command};
+use clap::{ArgAction, Parser, command};
 
-// #[derive(Parser)]
-// #[command(disable_help_flag = true)]
-// struct Args{
-//     #[arg(short, long, action = ArgAction::SetTrue)]
-//     version: bool,
+#[derive(Parser)]
+#[command(disable_help_flag = true)]
+struct Args{
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    version: bool,
      
-//      #[arg(short, long, action = ArgAction::SetTrue)]
-//     tcp: bool,
+     #[arg(short, long, action = ArgAction::SetTrue)]
+    tcp: bool,
 
-//      #[arg(short, long, action = ArgAction::SetTrue)]
-//     udp: bool,
+     #[arg(short, long, action = ArgAction::SetTrue)]
+    udp: bool,
 
-//     #[arg(short, long, action = ArgAction::SetTrue)]
-//     listen: bool,
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    listen: bool,
 
-//     #[arg(short, long, default_value_t = String::default())]
-//     host: String,
+    #[arg(short, long, default_value_t = String::default())]
+    host: String,
 
-//     #[arg(short, long, default_value_t = 0i32)]
-//     port: i32,
+    #[arg(short, long, default_value_t = 0i32)]
+    port: i32,
 
-//     #[arg(short, long, default_value_t = String::default())]
-//     message: String
-// }   
-
-// #[tokio::main]
-// async fn main() -> Result<(),Box<dyn std::error::Error>>{
-//     let args = Args::parse();
-
-//     if args.version{
-//         println!("v0.0.1");
-//     }
-
-//     if args.listen{
-//         listener::listen_from(&args.host,args.port, args.tcp, args.udp,args.message).await?;
-//     }
-
-//     Ok(())
-
-// }
-
-use axum::{routing::get, Router};
+    #[arg(short, long, default_value_t = String::default())]
+    message: String
+}   
 
 #[tokio::main]
-async fn main(){
-    let app: Router = Router::new()
-        .route("/", get(async || {"Hello, World!"}));
+async fn main() -> Result<(),Box<dyn std::error::Error>>{
+    let args = Args::parse();                                   
 
-    axum::serve(tokio::net::TcpListener::bind("127.0.0.1:2000").await.unwrap(),app.into_make_service()).await.unwrap();
+    if args.version{
+        println!("v0.0.1");
+    }
+
+    if args.listen{
+        listener::listening(args.host,args.port).await?;
+    }
+
+    Ok(())
+
 }
+
