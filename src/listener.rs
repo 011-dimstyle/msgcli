01cdb1a2223a901b398bbea04f7545e617c8959f
dimstyle <smilehacking27@gmail.com>
2025-09-22 18:33:12 +0700

    fix the reader handler for output

--- src/listener.rs
+++ src/listener.rs
@@ -1,64 +1,83 @@
-use std::sync::Arc;
+use core::net::SocketAddr;
 use tokio::sync::Mutex;
 use tokio::{
     io::{AsyncWriteExt, BufReader, AsyncBufReadExt},
-    net::{TcpListener},
+    net::{self, TcpListener},
 };
+
+async fn readhandler(readermutex: Mutex<net::tcp::OwnedReadHalf>, ipaddrmutex: Mutex<SocketAddr>) {
+    let mut readerguard = readermutex.lock().await;
+    let mut readerbuffer = BufReader::new(&mut *readerguard);
+    let mut output = tokio::io::stdout();
+    let mut readerstring = String::new();
+    let ipaddrguard = ipaddrmutex.lock().await;
+
+    loop {
+        println!("");
+        readerstring.clear();
+        readerbuffer.read_line(&mut readerstring).await.unwrap();
+
+        output
+            .write_all(format!("{} : {}", ipaddrguard, readerstring).as_bytes())
+            .await
+            .unwrap();
+        output.flush().await.unwrap();
+    }
+}
+
+async fn writehandler(writermutex: Mutex<net::tcp::OwnedWriteHalf>) {
+    let mut writerguard = writermutex.lock().await;
+    let localaddr = writerguard.local_addr().unwrap();
+    let mut output = tokio::io::stdout();
+    let input = tokio::io::stdin();
+    let readerbuffer = BufReader::new(input);
+    let mut lines = readerbuffer.lines();
+    loop {
+        output.flush().await.unwrap();
+        output
+            .write_all(format!("{} : ", localaddr).as_bytes())
+            .await
+            .unwrap();
+        output.flush().await.unwrap();
+        match lines.next_line().await.unwrap() {
+            Some(line) => {
+                writerguard
+                    .write_all(format!("{}\n", line).as_bytes())
+                    .await
+                    .unwrap();
+            }
+            None => {
+                println!("");
+                break;
+            }
+        }
+    }
+}
+
+async fn controlflowhandler(){
+    //to handle the stdout and stdin 
+}
+
 
 pub async fn listening(host: String, port: u32) -> Result<(), Box<dyn std::error::Error>> {
 
     let listen = TcpListener::bind(format!("{}:{}", host, port)).await?;
     println!("listen on {}", listen.local_addr()?);
     
-    let (streammutex, ipaddr) = match listen.accept().await{
-        Ok((stream,sock)) => (Arc::new(Mutex::new(stream)), Arc::new(Mutex::new(sock))),
-        Err(e) => {panic!("{}", e);}
+    let (writermutex, readermutex, ipaddrmutex) = match listen.accept().await {
+        Ok((stream, sock)) => {
+            let (reader, writer) = stream.into_split();
+            (Mutex::new(writer), Mutex::new(reader), Mutex::new(sock))
+        }
+        Err(e) => {
+            panic!("{}", e);
+        }
     };
     
-    let streamread = Arc::clone(&streammutex);
-    let streamwrite = Arc::clone(&streammutex);
-    let ipaddrmutex = Arc::clone(&ipaddr);
-    
-    let writerhandler = tokio::spawn(async move {
+    let writerfuture = tokio::spawn(writehandler(writermutex));
+    tokio::spawn(readhandler(readermutex, ipaddrmutex));
 
-        let mut streamguard = streamwrite.lock().await;
-        let input = tokio::io::stdin();
-        let mut output = tokio::io::stdout();
-        let readerbuffer = BufReader::new(input);
-        let mut lines = readerbuffer.lines();
-        loop{
-            output.write_all(format!("{} : ", streamguard.local_addr().unwrap()).as_bytes()).await.unwrap();
-            output.flush().await.unwrap();
-            match lines.next_line().await.unwrap() {
-                Some(mut line) => {
-                    line.push('\n');
-                    streamguard.write_all(line.as_bytes()).await.unwrap();
-                },
-                None => {
-                    println!("");
-                    break;
-                }
-            }
-        }   
-                                                                    
-    });
-        
-    tokio::spawn(async move {
-        println!("hello");
-        let mut streamguard = streamread.lock().await;
-        let mut readstring = String::new();
-        let mut readerbuffer = BufReader::new(&mut *streamguard);
-        let mut output = tokio::io::stdout();
-        let ipaddrguard = ipaddrmutex.lock().await;
-        loop{
-            // streamguard.read_to_string(&mut readbuffer).await.unwrap();
-            readerbuffer.read_line(&mut readstring).await.unwrap();
-            output.write_all(format!("{} : {} ", ipaddrguard ,readstring).as_bytes()).await.unwrap();
-            output.flush().await.unwrap();
-        }
-    });
-
-    writerhandler.await?;
+    writerfuture.await?;
 
     Ok(())
 
